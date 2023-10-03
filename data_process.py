74138bcb060387b95cc5aa4f6b52aa5c06e555de
Alyssa Zheng <jingwenz@andrew.cmu.edu>
2023-10-03 13:42:03 -0400

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[549]:
-
-
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,63 +10,25 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-
-# In[550]:
-
-
 user = pd.read_csv("user_metrics.csv")
-
-
-# In[553]:
-
-
 user['USER_LAST_SEEN_DATE'] = pd.to_datetime(user['USER_LAST_SEEN_DATE'])
 user['USER_FIRST_SEEN_DATE'] = pd.to_datetime(user['USER_FIRST_SEEN_DATE'])
-
-
-# In[554]:
-
-
 user['TIME_DIFF'] = (user['USER_LAST_SEEN_DATE'] - user['USER_FIRST_SEEN_DATE']).dt.days
-
-
-# In[555]:
-
-
 user['D7_RETENTION'] = user['TIME_DIFF'].apply(lambda x: 1 if x >= 7 else 0)
 
-
-# In[557]:
-
-
+#exploratory data analysis
 sns.countplot(data=user, x='D7_RETENTION')
 plt.title('Distribution of D7_RETENTION')
 plt.show()
 
-
-# In[558]:
-
-
 sns.histplot(data=user, x='EVENTAPP_LAUNCHCOUNT')
 plt.show()
-
-
-# In[559]:
-
 
 sns.boxplot(data=user, y='EVENTCLIENTDEVICECOUNT', x='D7_RETENTION')
 plt.show()
 
-
-# In[560]:
-
-
 sns.scatterplot(data=user, x='EVENTGAMESTARTEDCOUNT', y='EVENTADSHOWCOUNT', hue='D7_RETENTION')
 plt.show()
-
-
-# In[561]:
-
 
 correlation_matrix = user[['EVENTAPP_LAUNCHCOUNT', 'EVENTAPP_EXITCOUNT', 'EVENTAPP_FINISH_LOADINGCOUNT',
                         'EVENTCLIENTDEVICECOUNT', 'EVENTGAMESTARTEDCOUNT', 'EVENTADSHOWCOUNT',
@@ -79,21 +37,9 @@ correlation_matrix = user[['EVENTAPP_LAUNCHCOUNT', 'EVENTAPP_EXITCOUNT', 'EVENTA
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
 plt.show()
 
-
-# In[562]:
-
-
+#feature engineering
 features = ["EVENTAPP_LAUNCHCOUNT","EVENTAPP_EXITCOUNT","EVENTAPP_FINISH_LOADINGCOUNT","EVENTCLIENTDEVICECOUNT","EVENTGAMESTARTEDCOUNT","EVENTADSHOWCOUNT","EVENTNEWPLAYERCOUNT","EVENTTUTORIAL_STEP_COMPLETECOUNT","EVENTUI_POPUPCOUNT","TOTALDAYSPLAYED" ]
-
-
-# In[563]:
-
-
 user[features].isnull().sum()
-
-
-# In[565]:
-
 
 user["EVENTAPP_LAUNCHCOUNT"].fillna(user["EVENTAPP_LAUNCHCOUNT"].mean(), inplace = True)
 user["EVENTAPP_EXITCOUNT"].fillna(user["EVENTAPP_EXITCOUNT"].mean(), inplace = True)
@@ -106,33 +52,10 @@ user["EVENTUI_POPUPCOUNT"].fillna(user["EVENTUI_POPUPCOUNT"].mean(), inplace = T
 user["TOTALDAYSPLAYED"].fillna(user["TOTALDAYSPLAYED"].mean(), inplace = True)
 user["EVENTADSHOWCOUNT"].fillna(user["EVENTADSHOWCOUNT"].mean(), inplace = True)
 
-
-# In[568]:
-
-
+#set up y label
 target = 'D7_RETENTION'
-
-
-# In[569]:
-
-
 user[features]
-
-
-# In[570]:
-
-
 scaler = StandardScaler()
-
-
-# In[571]:
-
-
 scaler.fit(user[features])
-
-
-# In[572]:
-
-
 user[features] = scaler.transform(user[features])
 
